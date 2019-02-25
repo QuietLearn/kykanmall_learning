06a146f5c638774d2a07d439c50b01f03d9c1935
simpleY <784510436@qq.com>
2019-02-25 21:07:46 +0800

    ..

--- src/main/java/com/mmall/util/CookieUtil.java
+++ src/main/java/com/mmall/util/CookieUtil.java
@@ -1,17 +1,60 @@
 package com.mmall.util;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.http.HttpRequest;
-
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@Slf4j
 public class CookieUtil {
     //同级别的域名访问(读)不到同级别域名domain下的cookie
     private final static String COOKIE_DOMAIN=".happymmall.com";
     private final static String COOKIE_NAME="mmall_login_token";
     //token就是cookie--jsessionId
 
-    public static void writeCookie(HttpServletRequest request, HttpServletResponse response){
+    public static void writeLoginToken(HttpServletResponse response,String token){
+        Cookie cookie = new Cookie(COOKIE_NAME,token);
+        cookie.setDomain(COOKIE_DOMAIN);
+        //代表设置在根目录
+        cookie.setPath("/");
+        //单位是秒。
+        //如果这个maxage不设置的话，cookie就不会写入硬盘，而是写在内存。只在当前页面有效。
+        cookie.setMaxAge(60 * 60 * 24 * 365);//如果是-1，代表永久
+        log.info("write cookieName:{},cookieValue:{}",cookie.getName(),cookie.getValue());
+        response.addCookie(cookie);
+    }
+
+    public static String readLoginLoken(HttpServletRequest request){
+        Cookie[] cookies = request.getCookies();
+        if (cookies!=null){
+            for (Cookie cookie:cookies) {
+                log.info("read cookieName:{},cookieValue:{}",cookie.getName(),cookie.getValue());
+                if (StringUtils.equals(cookie.getName(),COOKIE_NAME)){
+                    log.info("return cookieName:{},cookieValue:{}",cookie.getName(),cookie.getValue());
+                    return cookie.getValue();
+                }
+            }
+        }
+       return null;
+    }
+
+    public static void delLoginToken(HttpServletRequest request,HttpServletResponse response){
+        Cookie[] cookies = request.getCookies();
+        if (cookies!=null){
+            for (Cookie cookie:cookies) {
+                log.info("read cookieName:{},cookieValue:{}",cookie.getName(),cookie.getValue());
+                if (StringUtils.equals(cookie.getName(),COOKIE_NAME)){
+                    cookie.setDomain(COOKIE_DOMAIN);
+                    cookie.setPath("/");
+                    cookie.setMaxAge(0);//0就是没有生存时间
+                    log.info("del cookieName:{},cookieValue:{}",cookie.getName(),cookie.getValue());
+                    response.addCookie(cookie);
+                    return;
+                }
+            }
+        }
 
     }
 }
